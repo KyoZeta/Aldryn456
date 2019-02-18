6db19c4241ff3bbe53cfb3aaa28467ff4d2ac0da
KyoZeta <47518944+KyoZeta@users.noreply.github.com>
2019-02-18 10:57:18 -0600

    Tareea

---
+++ WebMVCPrueba/WebMVCPrueba/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVCPRUEBA.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "HOLA MUNDO ME LA CHUPAN";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult JuegosEroticos()
+        {
+            ViewBag.Message = "SEÑORA SU HIJO ESTA VIENDO PORNO";
+
+            return View();
+        }
+    }
+}
